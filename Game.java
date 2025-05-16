9cb0aad99501dc878e8543dd862c2c2597db8194
DONUT0909 <109625633+DONUT0909@users.noreply.github.com>
2025-05-16 23:26:13 +0000

    Final project: Dungeon Crawler with report and flow charts

---
+++ Game.java
@@ -0,0 +1,7 @@
+//Main entry point of the game
+public class Game {
+    public static void main(String[] args) {
+        GameManager gameManager = new GameManager(); //Createss the insstance to control the game
+        gameManager.startGame();
+    }
+}
